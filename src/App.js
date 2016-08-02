85af74bfd02e4e056bec661ee8b2acc9c1601125
Christoph Reimers <christoph@superservice-international.com>
2016-08-02 21:35:42 +0200

    a little bit of everything

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+import FilterableNotesList from './modules/FilterableNotesList';
+
 import './App.css';
+
+var NOTES = [
+  {title: "Nicht vergessen", note: "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."},
+  {title: "Ganz wichtig", note: "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."},
+  {title: "Geburtstagsgeschenk Mama", note: "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."},
+];
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <h1>Test</h1>
+          <AppBar
+            title="mental notes"
+            iconElementRight={
+              <IconMenu
+                iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              >
+                <MenuItem primaryText="Sign out" />
+              </IconMenu>
+            }
+          />
+
+          <FilterableNotesList/>
+
         </div>
       </MuiThemeProvider>
     );
