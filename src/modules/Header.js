b94d2452863c9f2a3e17ceaf7bd6eb84e1955556
Christoph Reimers <christoph@superservice-international.com>
2016-08-04 14:51:39 +0200

    linke app bar title to /

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter  } from 'react-router'
 
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
@@ -8,10 +9,21 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.handleTitleTap = this.handleTitleTap.bind(this);
+  }
+
+  handleTitleTap() {
+    this.props.router.push('/');
+  }
+
   render() {
     return (
       <AppBar
         title="mental notes"
+        onTitleTouchTap={this.handleTitleTap}
         iconElementRight={
           <IconMenu
             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
@@ -26,4 +38,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default withRouter(Header)
